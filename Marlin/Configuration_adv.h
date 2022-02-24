ec848d52989c2106611c3f3a93ccbfd84f94484e
HotWire CNC <hotwire.cnc@gmail.com>
2022-02-24 20:38:32 +0000

    Typo in custom menu

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3987,7 +3987,7 @@
 // Custom Menu: Main Menu
 #define CUSTOM_MENU_MAIN
 #if ENABLED(CUSTOM_MENU_MAIN)
-  #define CUSTOM_MENU_MAIN_TITLE "Set/Move Axe to Zero"
+  #define CUSTOM_MENU_MAIN_TITLE "Set/Move Axes to Zero"
   #define CUSTOM_MENU_MAIN_SCRIPT_DONE "Done"
   //#define CUSTOM_MENU_MAIN_SCRIPT_AUDIBLE_FEEDBACK
   #define CUSTOM_MENU_MAIN_SCRIPT_RETURN   // Return to status screen after a script
